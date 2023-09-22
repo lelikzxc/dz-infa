7cf68e5d1ebe62e4f01a24d6ad9d1fd470b7e6bd
лёлик <145128494+lelikzxc@users.noreply.github.com>
2023-09-22 18:01:59 +0300

    Add files via upload

---
+++ upr6.py
@@ -0,0 +1,20 @@
+def mnk(x, y):
+    if len(x)!=len(y):
+        return None
+    x_mean=sum(x)/len(x)
+    y_mean=sum(y)/len(y)
+    numerator=0
+    denominator=0
+    for i in range(len(x)):
+        numerator+=(x[i]-x_mean)*(y[i]-y_mean)
+        denominator+=(x[i]-x_mean)**2
+        a=numerator/denominator
+        b=y_mean-a*x_mean
+        return a, b
+x=list(map(float, input().split()))
+y=list(map(float, input().split()))
+for i in range(len(x)):
+    x[i]=int(x[i])
+    y[i]=int(y[i])
+a, b=mnk(x, y)
+print(a,b)
