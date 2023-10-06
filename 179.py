6791a6ea9c22d002392ba8837d8f3443d0a25d69
лёлик <145128494+lelikzxc@users.noreply.github.com>
2023-10-06 19:05:04 +0300

    Update 179.py

--- 179.py
+++ 179.py
@@ -1,46 +1,33 @@
-a = list(map(str, input().split()))
 
-def bm(a, b):
-    if a==b:
-        return 0
-    elif a.startswith(b):
-        return bm(a[len(b):],b)
-    elif b.startswith(a):
-        return bm(a, b[len(a):])
-    else:
-        return -1 if a<b else 1
-def max_nums(s):
-    num=s
-    num.sort(key=lambda x:x, reverse=True, cmp=bm)
-    return "".join(num)
-
-print(max_nums(a))
-
-# Функция, которая сравнивает два числа по их строковому представлению
 def compare(a, b):
-  # Если числа равны, то возвращаем 0
-  if a == b:
-    return 0
-  # Если первое число начинается со второго, то сравниваем оставшиеся части
-  elif a.startswith(b):
-    return compare(a[len(b):], b)
-  # Если второе число начинается с первого, то сравниваем оставшиеся части
-  elif b.startswith(a):
-    return compare(a, b[len(a):])
-  # Иначе сравниваем числа лексикографически
+  a = str(a)
+  b = str(b)
+  if a[0] > b[0]:
+    return 1 
+  elif a[0] < b[0]:
+    return -1 
   else:
-    return -1 if a < b else 1
+    if len(a) > len(b):
+      return compare(a, b + b[0])
+    elif len(a) < len(b):
+      return compare(a + a[0], b)
+    else:
 
-# Функция, которая получает на вход строку целых неотрицательных чисел и составляет из них наибольшее возможное число
-def max_number(s):
-  # Разбиваем строку на список чисел
-  numbers = s.split()
-  # Сортируем список по убыванию с помощью функции compare
-  numbers.sort(key=lambda x: x, reverse=True, cmp=compare)
-  # Соединяем отсортированные числа в одну строку и возвращаем ее
-  return "".join(numbers)
+      return 0
 
-# Пример ввода и вывода
-s = "3 30 34 5 9"
-print(max_number(s)) # Вывод: 9534330
+def sort(nums):
+  for i in range(len(nums) - 1):
+    for j in range(len(nums) - i - 1):
+      if compare(nums[j], nums[j+1]) == -1:
+        nums[j], nums[j+1] = nums[j+1], nums[j]
+  return nums
 
+def max_number(nums):
+  nums = sort(nums)
+  result = ""
+  for num in nums:
+    result += str(num)
+  print(result)
+
+nums = [3, 30, 34, 5, 9]
+max_number(nums)
